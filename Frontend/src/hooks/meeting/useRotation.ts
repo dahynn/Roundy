6094124131c03df06a62f2fa05fad1a3d24a3036
kim beom seung <kim3210542@gmail.com>
2026-02-07 18:51:53 +0900

    Hotfix : check VITE_WS_URL

--- Frontend/src/hooks/meeting/useRotation.ts
+++ Frontend/src/hooks/meeting/useRotation.ts
@@ -296,10 +296,10 @@ export const useRotationSystem = (roomId: string | null, token: string | null, u
 
         // 사용자 요청: ws://localhost:8080/ws/webrtc?token=
         const baseUrl = import.meta.env.VITE_WS_URL;
+        console.log('baseUrl : ' + baseUrl);
+        console.log('VITE_WS_URL : ' + import.meta.env.VITE_WS_URL);
         if (!baseUrl || baseUrl === 'undefined') {
-            console.error('[WS] VITE_WS_URL 환경변수가 설정되지 않았습니다.');
-            console.log("baseUrl : " + baseUrl);
-            console.log("VITE_WS_URL : " + import.meta.env.VITE_WS_URL);
+          console.error('[WS] VITE_WS_URL 환경변수가 설정되지 않았습니다.');
             return;
         }
 
